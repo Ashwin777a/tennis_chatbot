3bba6e233b1ce7cef5d7aa75993f123bb89bc9f4
Ashwin777a <ashwin777a@gmail.com>
2026-04-26 19:12:27 +0530

    third commit

--- app_standalone.py
+++ app_standalone.py
@@ -278,7 +278,3 @@ st.divider()
 col1, col2, col3 = st.columns(3)
 with col1:
     st.caption("🤖 Powered by AI")
-with col2:
-    st.caption("🎾 CE Tennis Academy")
-with col3:
-    st.caption(f"💬 {len(st.session_state.messages)} messages")
